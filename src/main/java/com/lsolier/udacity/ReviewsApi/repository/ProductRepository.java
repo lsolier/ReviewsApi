7752a9dc51b8c0faa16d756afd93c2250290449c
Luis Solier Sajami <luis.sajami@gmail.com>
2019-11-30 19:47:13 -0500

    adding JPQL

--- src/main/java/com/lsolier/udacity/ReviewsApi/repository/ProductRepository.java
+++ src/main/java/com/lsolier/udacity/ReviewsApi/repository/ProductRepository.java
@@ -2,8 +2,15 @@ package com.lsolier.udacity.ReviewsApi.repository;
 
 import com.lsolier.udacity.ReviewsApi.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+  @Query("SELECT p FROM Product p WHERE p.productname LIKE '%NUEVA%'")
+  Collection<Product> findAllProductWhereProductNameContainsNueva();
+
 }
--- src/main/java/com/lsolier/udacity/ReviewsApi/repository/ReviewRepository.java
+++ src/main/java/com/lsolier/udacity/ReviewsApi/repository/ReviewRepository.java
@@ -2,6 +2,15 @@ package com.lsolier.udacity.ReviewsApi.repository;
 
 import com.lsolier.udacity.ReviewsApi.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+
+  @Query("SELECT r FROM Review r WHERE r.description LIKE '%LED%'")
+  Collection<Review> findAllReviewWhereProductNameContainsLed();
+
 }
